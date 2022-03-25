c943bd59fbea1a5e136064a87f1563a7b08c0a92
Aku Paananen <aku.k.paananen@utu.fi>
2022-03-25 10:59:41 +0200

    Lisätty placeholder tiedostoja

---
+++ src/main/java/algorithmcomparison/Main.java
@@ -0,0 +1,10 @@
+package algorithmcomparison;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/algorithmcomparison/AlgorithmTests.java
@@ -0,0 +1,5 @@
+package algorithmcomparison;
+
+public class AlgorithmTests {
+
+}
