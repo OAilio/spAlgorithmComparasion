7c1c7464aa25fd2a76c3b02a87fc666e4543e761
Okko Ailio <oiaili@utu.fi>
2022-04-19 11:26:15 +0300

    Fixed issues with quicksort class

--- src/main/java/algorithmcomparison/Quicksort.java
+++ src/main/java/algorithmcomparison/Quicksort.java
@@ -4,54 +4,67 @@ package algorithmcomparison;
  * Quicksort
  */
 public class Quicksort {
-	
-	/**
-	 * Sorts the dataset using quicksort
-	 * 
-	 * @param arr The unsorted dataset
-	 * 
-	 * @return arr
-	 */
+    
+    /**
+     * Sorts the dataset using quicksort
+     * 
+     * @param arr The unsorted dataset
+     * 
+     * @return arr
+     */
     public static int[] quickSort(int[] arr) {
-    	//Clones the array
-    	int[] array = arr.clone();
-    	
+        //Clones the array
+        int[] array = arr.clone();
+        
         int low = 0;
         int high = array.length -1;
 
+        sort(arr,low,high);
+        return arr;
+    }   
+    /**
+     * sorts the cloned dataset
+     * 
+     * @param array 
+     * @param low
+     * @param high
+     */
+    static void sort(int array[], int low, int high ){
         //Determine the pivot value in the middle of the array
-		int middle = low + (high - low) / 2;
-		int pivot = array[middle];
+        int middle = low + (high - low) / 2;
+        int pivot = array[middle];
 
-		int i = low, j = high;
-		while (i <= j) {
+        int i = low, j = high;
+        while (i <= j) {
 
             //if index i is greater than the pivot, increase i's value
-			while (array[i] < pivot) {
-				i++;
-			}
+            while (array[i] < pivot) {
+                i++;
+            }
 
             //if index j is greater than the pivot value, increase j's value
-			while (array[j] > pivot) {
-				j--;
-			}
+            while (array[j] > pivot) {
+                j--;
+            }
             
-			if (i <= j) {
+            if (i <= j) {
                 //Memorize the greater value
-				int memory = array[i];
+                int memory = array[i];
                 //Put the smaller element in the smaller index
-				array[i] = array[j];
+                array[i] = array[j];
                 //Insert the greater value to its new index from the memory
-				array[j] = memory;
+                array[j] = memory;
 
                 //Raise i's value
-				i++;
+                i++;
                 //Decrease j's value
-				j--;
-			}
+                j--;
+            }
         }
-        
-        //Return the sorted array
-        return array;
+
+        if (low < j)
+                sort(array,low,j);
+        if (high > i)
+                sort(array,i,high);
     }
 }
